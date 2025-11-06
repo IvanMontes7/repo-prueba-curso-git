2abd94a52e04baf9063eed34d9b626c51b2e9fcb
Ivan Montes <ivanmontes.3389@gmail.com>
2025-11-06 15:18:13 -0600

    subiendo las modificaciones a github

--- func.py
+++ func.py
@@ -1,4 +1,8 @@
 def suma(*num):
     return sum(num)
 
-print(suma(2,3,4))
+print(suma(2,3,4,5,6,7))
+
+# Prueba de modificaciones para practicasr con github. 
+# Este es un repo clonado de github y se acaban de hacer modificaciones
+# en el print, agregando más numeros para sumar 
